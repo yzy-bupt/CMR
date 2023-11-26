e0853937e79cf5ebca84e72e853430eb7339e440
yzy-bupt <1434620877@qq.com>
2023-11-26 15:32:09 +0000

    load

--- load_data.py
+++ load_data.py
@@ -4,6 +4,10 @@ from scipy.io import loadmat, savemat
 from torch.utils.data import DataLoader
 from util import BackgroundGenerator
 import numpy as np
+from transformers import CLIPProcessor, CLIPModel
+from PIL import Image
+import os
+from pycocotools.coco import COCO
 
 
 class DataLoaderX(DataLoader):
@@ -45,12 +49,114 @@ class SingleModalDataSet(Dataset):
 
 
 def get_loader(path, batch_size, INCOMPLETE=False, USE_INCOMPLETE=False):
-    img_train = loadmat(path + "train_img.mat")['train_img']
-    img_test = loadmat(path + "test_img.mat")['test_img']
-    text_train = loadmat(path + "train_txt.mat")['train_txt']
-    text_test = loadmat(path + "test_txt.mat")['test_txt']
-    label_train = loadmat(path + "train_lab.mat")['train_lab']
-    label_test = loadmat(path + "test_lab.mat")['test_lab']
+    # data_mat = loadmat("labels.COCO.mat")
+    # lab_list = data_mat['labels']
+    # id_list = data_mat['id']
+    # # img_list = data_mat['img_name']
+
+    # caps_file_train = os.path.join("annotations", "captions_train2014.json")
+    # caps_file_val = os.path.join("annotations", "captions_val2014.json")
+
+    # coco_caps_train = COCO(caps_file_train)
+    # coco_caps_val = COCO(caps_file_val)
+
+    # model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14").cuda()
+    # processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+    # path = 'datapath_to_coco/'
+
+    # img_list = []
+    # txt_list = []
+    # cnt = 0
+    # for idx in id_list:
+    #     idx = int(idx)
+    #     _annIds = coco_caps_train.getAnnIds(imgIds=idx)
+    #     if len(_annIds):
+    #         _anns = coco_caps_train.loadAnns(_annIds)
+    #     else:
+    #         _annIds = coco_caps_val.getAnnIds(imgIds=idx)
+    #         _anns = coco_caps_val.loadAnns(_annIds)
+        
+    #     sentences = [_a["caption"] for _a in _anns]
+
+    #     if os.path.exists(path + 'train2014/COCO_train2014_' + str(idx).zfill(12) + '.jpg'):
+    #         img = Image.open(path + 'train2014/COCO_train2014_' + str(idx).zfill(12) + '.jpg')
+    #     else:
+    #         img = Image.open(path + 'val2014/COCO_val2014_' + str(idx).zfill(12) + '.jpg')
+
+    #     inputs = processor(text=sentences, images=img, return_tensors="pt", padding=True).to(torch.device("cuda:0"))
+
+    #     outputs = model(**inputs)
+    #     img_list.append(outputs['image_embeds'].cpu().detach().numpy())
+    #     txt_list.append(outputs['text_embeds'].mean(dim=0)[None,:].cpu().detach().numpy())
+    #     cnt += 1
+    #     print(cnt)
+
+    # img_list = np.concatenate(img_list)
+    # txt_list = np.concatenate(txt_list)
+
+    # shuffle_idx = np.arange(img_list.shape[0])
+    # np.random.shuffle(shuffle_idx)
+    # img_list = img_list[shuffle_idx]
+    # txt_list = txt_list[shuffle_idx]
+    # lab_list = lab_list[shuffle_idx]
+
+    # img_train = img_list[2000:]
+    # img_test = img_list[:2000]
+    # text_train = txt_list[2000:]
+    # text_test = txt_list[:2000]
+    # label_train = lab_list[2000:]
+    # label_test = lab_list[:2000]
+
+    # savemat("data/train.mat", {'img_train':img_train, 'txt_train':text_train, 'lab_train':label_train}, do_compression=True)
+    # savemat("data/test.mat", {'img_test':img_test, 'txt_train':text_test, 'lab_train':label_test}, do_compression=True)
+    
+    if 0:
+        mat_train = loadmat("model/train.mat")
+        mat_test = loadmat("model/test.mat")
+        
+        img_train = mat_train['img_train']
+        text_train = mat_train['txt_train']
+        label_train = mat_train['lab_train']
+
+        img_test = mat_test['img_test']
+        text_test = mat_test['txt_test']
+        label_test = mat_test['lab_test']
+    else:
+        mat_train = loadmat(path + "train_img.mat")
+        img_train = mat_train['train_img']
+        mat_test = loadmat(path + "test_img.mat")
+        img_test = mat_test['test_img']
+        text_train = loadmat(path + "train_txt.mat")['train_txt']
+        text_test = loadmat(path + "test_txt.mat")['test_txt']
+        label_train = loadmat(path + "train_lab.mat")['train_lab']
+        label_test = loadmat(path + "test_lab.mat")['test_lab']
+
+    # tr_lab = loadmat(path + "train_lab.mat")['train_lab']
+    # tr_id = loadmat(path + "train_lab.mat")['id'][0]
+    # tt_lab = loadmat(path + "test_lab.mat")['test_lab']
+    # tt_id = loadmat(path + "test_lab.mat")['id'][0]
+    # c_lab = loadmat("labels.COCO.mat")['labels']
+    # c_name = loadmat("labels.COCO.mat")['img_name']
+
+    # cnt = 0
+    # for id in tr_id:
+    #     idx = np.where(tr_id == cnt)[0][0]
+    #     if (c_lab[cnt] == tr_lab[idx]).all() == False:
+    #         print('!!!')
+    #         break
+    #     cnt+=1
+    
+    # cnt = 0
+    # for id in tt_id:
+    #     if (c_lab[id] == tt_lab[cnt]).all() == False:
+    #         print('!!!')
+    #         break
+    #     cnt += 1
+
+
+    # id_train = mat_train['id'][0]
+    # id_test = mat_test['id'][0]
+
 
     # Incomplete modal
     split = img_train.shape[0] // 5
